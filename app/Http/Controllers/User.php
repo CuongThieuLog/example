17332dd1c23a0a2b8a10f76d4d439ff3a6f89e24
CuongThieu <cuongtt@solashi.com>
2024-07-30 15:40:28 +0700

    update

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class User extends Controller
 {
     public function index() {
-        return 123;
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'role' => 'admin'
+        ];
+
+        return response()->json($data);
     }
 }
